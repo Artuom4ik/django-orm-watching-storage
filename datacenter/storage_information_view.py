a1a2b589b38ab4af663974b1347d429ed70d717d
Artuom4ik <masterskayaigr123@gmail.com>
2023-03-12 12:33:08 +0300

    Остановил вирус среди рабочих.

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -9,6 +9,10 @@ from datacenter.models import Visit
 from django.shortcuts import render
 
 
+def is_visit_long(duration, minutes=60):
+        return duration // 60 > minutes
+
+
 def get_duration(visit, passcard_visit):
     entered_at = localtime(passcard_visit.entered_at)
     date_now = localtime(datetime.datetime.now(timezone.utc))
@@ -30,12 +34,12 @@ def storage_information_view(request):
     for passcard_visit in passcards_visit:
         visit = passcard_visit.entered_at
         duration = get_duration(visit, passcard_visit)
-        visits = {
+        non_closed_visits.append({
             'who_entered': f'{passcard_visit.passcard}',
             'entered_at': f'{passcard_visit.entered_at}',
             'duration': f'{format_duration(duration)}',
-        }
-        non_closed_visits.append(visits)
+            'is_strange': is_visit_long(duration),
+        })
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
